6cca8b15ef1b80ae8b904b7510d2c49f93fe4376
JiHun Kim <dydekffrl@naver.com>
2025-03-06 19:07:36 +0900

    refactor : 리팩토링 진행

--- src/main/java/synerjs/lookkit2nd/user/UserController.java
+++ src/main/java/synerjs/lookkit2nd/user/UserController.java
@@ -11,16 +11,20 @@ public class UserController {
 
     private final UserService userService;
 
-
+    //아이디 중복 체크
     @GetMapping("/check-id")
-    public ResponseEntity checkIdDuplication(@RequestParam String userUuid) {
+    public ResponseEntity<String> checkIdDuplication(@RequestParam String userUuid) {
         User result = userService.findByUserUuid(userUuid);
+
         if (result == null) {
             return ResponseEntity.status(200).body("사용 가능한 아이디");
         }
-        return ResponseEntity.status(500).body("중복 아이디");
+
+        return ResponseEntity.status(409).body("중복 아이디");
     }
 
+
+    // 유저정보 가져오기
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserInfo(@PathVariable Long id) {
         User user = userService.getUserInfo(id);
@@ -28,25 +32,29 @@ public class UserController {
     }
 
 
-    @PostMapping("/find/id")
-    public ResponseEntity findUserId(@RequestBody UserDTO userDTO) {
 
+    // 아이디로 유저 찾기
+    @PostMapping("/find/id")
+    public ResponseEntity<String> findUserId(@RequestBody UserDTO userDTO) {
         String result = userService.findById(userDTO);
 
         if ("결과없음".equals(result)) {
             return ResponseEntity.status(500).body("일치하는 유저아이디가 없습니다.");
         }
+
         return ResponseEntity.status(200).body(result);
 
     }
 
-
+    // 비밀번호 재설정
     @PostMapping("/update/password")
-    public ResponseEntity updatePassword(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<String> updatePassword(@RequestBody UserDTO userDTO) {
         String result = userService.updatePassword(userDTO);
+
         if ("실패".equals(result)) {
             return ResponseEntity.status(500).body("비밀번호 변경에 실패했습니다.");
         }
+
         return ResponseEntity.status(200).body(result);
 
     }
