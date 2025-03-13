d1c7c76fa21851d24463591b6048dd5c151a6104
JiHun Kim <dydekffrl@naver.com>
2025-03-13 23:04:53 +0900

    refactor : sout제거& log추가

--- src/main/java/synerjs/lookkit2nd/common/exception/GlobalExceptionHandler.java
+++ src/main/java/synerjs/lookkit2nd/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package synerjs.lookkit2nd.common.exception;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 //모든 컨트롤러에서 발생하는 예외를 전역으로 처리함
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
 
