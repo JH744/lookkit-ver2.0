729e7767179209b3a3bacc2f1c38ba20d6a37991
JiHun Kim <dydekffrl@naver.com>
2025-03-22 00:30:28 +0900

    fix : 다단계 경로 포워딩하도록 재수정

--- src/main/java/synerjs/lookkit2nd/common/controller/HomeController.java
+++ src/main/java/synerjs/lookkit2nd/common/controller/HomeController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    // 루트("/") 요청도 index.html로 포워딩
+    @GetMapping("/")
+    public String root() {
+        return "forward:/index.html";
+    }
+
     @GetMapping("/{path:[^\\.]*}/**")
     public String forward() {
         return "forward:/index.html";
